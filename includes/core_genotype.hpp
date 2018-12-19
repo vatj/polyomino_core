98e8997ee74533aaecf55ea6dad8ffe8b974ec77
Alex Leonard <tkom343@gmail.com>
2018-12-19 17:20:26 +0000

    empty edges bug

--- includes/core_genotype.hpp
+++ includes/core_genotype.hpp
@@ -3,15 +3,12 @@
 #include <cstdint>
 #include <vector>
 #include <algorithm>
-#include <utility>
-#include <numeric>
 #include <tuple>
 #include <set>
 
 #include <array>
 #include <random>
 
-#include <iostream>
 
 extern thread_local std::mt19937 RNG_Engine;
 
@@ -64,11 +61,12 @@ public:
 
   static inline std::vector<int8_t> AssemblePolyomino(const std::vector<std::pair<InteractionPair,double> > edges, std::set<InteractionPair>& interacting_indices) {
     //uint16_t accumulated_time=0;
+    if(edges.empty())
+      return {0,0,1};
     auto max_subunit = std::max_element(edges.begin(), edges.end(),[](const auto& left, const auto& right){return left.first.second <  right.first.second;})->first.second;
    
     const int8_t seed = 1+Q::free_seed*4*std::uniform_int_distribution<uint8_t>(0,max_subunit/4)(RNG_Engine);
     const size_t UNBOUND_LIMIT= 12*(max_subunit/4+1)*(max_subunit/4+1);
-    std::cout<<UNBOUND_LIMIT<<std::endl;
 
     std::vector<int8_t> placed_tiles{0,0,seed},growing_perimeter;
     std::vector<double> strengths_cdf;
